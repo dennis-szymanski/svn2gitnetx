3198e8620f05845cd641b9e05df36db7e88cfaff
Seth Hendrick <seth@shendrick.net>
2020-04-11 16:18:02 -0400

    Added unit tests to the GcErrorIgnorer.

---
+++ tests/unittests/GcErrorIgnorerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Moq;
+using Xunit;
+
+namespace Svn2GitNetX.Tests
+{
+    public class GcErrorIgnorerTests
+    {
+        // ---------------- Tests ----------------
+
+        [Fact]
+        public void DeleteGcLogIfEnabledWhileEnabledTest()
+        {
+            // Prepare
+            Options options = new Options
+            {
+                IgnoreGcErrors = true
+            };
+
+            string expectedPath = Path.Combine( ".", ".git", "gc.log" );
+
+            Mock<IFileSystem> mockFileSystem = new Mock<IFileSystem>( MockBehavior.Strict );
+            mockFileSystem.Setup( m => m.DeleteFileIfItExists( expectedPath ) );
+
+            GcErrorIgnorer uut = new GcErrorIgnorer( null, options, mockFileSystem.Object );
+
+            // Act
+            uut.DeleteGcLogIfEnabled();
+
+            // Assert
+            mockFileSystem.Verify( m => m.DeleteFileIfItExists( expectedPath ) );
+        }
+
+        [Fact]
+        public void DeleteGcLogIfEnabledWhileDisabledTest()
+        {
+            // Prepare
+            Options options = new Options
+            {
+                IgnoreGcErrors = false
+            };
+
+            Mock<IFileSystem> mockFileSystem = new Mock<IFileSystem>( MockBehavior.Strict );
+            mockFileSystem.Setup( m => m.DeleteFileIfItExists( It.IsAny<string>() ) );
+
+            GcErrorIgnorer uut = new GcErrorIgnorer( null, options, mockFileSystem.Object );
+
+            // Act
+            uut.DeleteGcLogIfEnabled();
+
+            // Assert
+            mockFileSystem.VerifyNoOtherCalls(); // Nothing should be called.
+        }
+    }
+}
