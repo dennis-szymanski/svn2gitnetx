19a2310690a03ccb040483d9a54c197ec5c29770
Seth Hendrick <seth@shendrick.net>
2020-04-11 15:17:08 -0400

    Added more unit tests for the StaleSvnBranchDeleter.

--- tests/unittests/StaleSvnBranchDeleter.cs
+++ tests/unittests/StaleSvnBranchDeleter.cs
@@ -117,6 +117,143 @@ namespace Svn2GitNetX.Tests
             Assert.Equal( expectedBranches, actualBranches );
         }
 
+        /// <summary>
+        /// If the SVN branches match the git branches exactly,
+        /// nothing should be purged.
+        /// </summary>
+        [Fact]
+        public void GetGitBranchesToPurgeWithSameBranchesTest()
+        {
+            // Prepare
+            List<string> svnBranches = new List<string>
+            {
+                ".something",
+                "with a space",
+                "something.else",
+                "normal"
+            };
+
+            List<string> gitBranches = new List<string>
+            {
+                "%2Esomething", // Git replaces the first '.' with %2E.
+                "with%20a%20space", // All spaces become %20
+                "something.else", // '.' in the middle is left alone
+                "normal" // normal branch name
+            };
+
+            MetaInfo info = new MetaInfo { LocalBranches = gitBranches };
+
+            Options options = new Options
+            {
+                Branches = new List<string> { "branches" },
+            };
+
+            StaleSvnBranchDeleter uut = new StaleSvnBranchDeleter(
+                svnUrl,
+                options,
+                null,
+                null,
+                null,
+                info
+            );
+
+            // Act
+            IEnumerable<string> branchesToPurge = uut.GetGitBranchesToPurge( svnBranches );
+
+            // Assert
+            Assert.Empty( branchesToPurge );
+        }
+
+        /// <summary>
+        /// If there are no SVN branches, purge all local branches.
+        /// </summary>
+        [Fact]
+        public void PurgeAllGitBranchesTest()
+        {
+            // Prepare
+            List<string> svnBranches = new List<string>();
+
+            List<string> gitBranches = new List<string>
+            {
+                "%2Esomething", // Git replaces the first '.' with %2E.
+                "with%20a%20space", // All spaces become %20
+                "something.else", // '.' in the middle is left alone
+                "normal" // normal branch name
+            };
+
+            MetaInfo info = new MetaInfo { LocalBranches = gitBranches };
+
+            Options options = new Options
+            {
+                Branches = new List<string> { "branches" },
+            };
+
+            StaleSvnBranchDeleter uut = new StaleSvnBranchDeleter(
+                svnUrl,
+                options,
+                null,
+                null,
+                null,
+                info
+            );
+
+            // Act
+            IEnumerable<string> branchesToPurge = uut.GetGitBranchesToPurge( svnBranches );
+
+            // Assert
+            Assert.Equal( gitBranches, branchesToPurge ); // <- All branches should be purged.
+        }
+
+        /// <summary>
+        /// If there are no SVN branches, purge all local branches.
+        /// </summary>
+        [Fact]
+        public void PurgeSomeGitBranchesTest()
+        {
+            // Prepare
+            List<string> svnBranches = new List<string>
+            {
+                "normal"
+            };
+
+            List<string> gitBranches = new List<string>
+            {
+                "%2Esomething", // Git replaces the first '.' with %2E.
+                "with%20a%20space", // All spaces become %20
+                "something.else", // '.' in the middle is left alone
+                "normal" // normal branch name
+            };
+
+            List<string> expectedBranchesToPurge = new List<string>
+            {
+                "%2Esomething", // Git replaces the first '.' with %2E.
+                "with%20a%20space", // All spaces become %20
+                "something.else", // '.' in the middle is left alone
+            };
+
+            MetaInfo info = new MetaInfo { LocalBranches = gitBranches };
+
+            Options options = new Options
+            {
+                Branches = new List<string> { "branches" },
+            };
+
+            StaleSvnBranchDeleter uut = new StaleSvnBranchDeleter(
+                svnUrl,
+                options,
+                null,
+                null,
+                null,
+                info
+            );
+
+            // Act
+            IEnumerable<string> branchesToPurge = uut.GetGitBranchesToPurge( svnBranches );
+
+            // Assert
+            Assert.Equal( expectedBranchesToPurge, branchesToPurge ); // <- All branches should be purged except normal.
+        }
+
         // ---------------- Test Helpers ----------------
 
         private void SetupMockForVersionQuery( Mock<ICommandRunner> mock, int exitCode = 0 )
